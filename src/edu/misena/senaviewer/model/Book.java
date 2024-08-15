4b5a11091a0a61fa20aad402bafbafe39c16a04e
Juan Carlos Murillo Quintero <juancarlosmurilloquintero13@gmail.com>
2024-08-15 09:37:55 -0500

    First Commit

--- src/edu/misena/senaviewer/model/Book.java
+++ src/edu/misena/senaviewer/model/Book.java
@@ -1,2 +1,90 @@
-package edu.misena.senaviewer.model;public class Book {
-}
+package edu.misena.senaviewer.model;
+import java.util.List;
+public class Book{
+        // Atributos
+        private int id;
+        private String title;
+        private String editionDate;
+        private String editorial;
+        private List<String> authors;
+        private String isbn;
+        private boolean readed;
+        private int timeReaded;
+
+        // Constructor
+        public Book(String title, String editionDate, String editorial, String isbn) {
+            this.title = title;
+            this.editionDate = editionDate;
+            this.editorial = editorial;
+            this.isbn = isbn;
+            this.readed = false;
+            this.timeReaded = 0;
+        }
+
+        // Getters y Setters
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getEditionDate() {
+            return editionDate;
+        }
+
+        public void setEditionDate(String editionDate) {
+            this.editionDate = editionDate;
+        }
+
+        public String getEditorial() {
+            return editorial;
+        }
+
+        public void setEditorial(String editorial) {
+            this.editorial = editorial;
+        }
+
+        public List<String> getAuthors() {
+            return authors;
+        }
+
+        public void setAuthors(List<String> authors) {
+            this.authors = authors;
+        }
+
+        public String getIsbn() {
+            return isbn;
+        }
+
+        public void setIsbn(String isbn) {
+            this.isbn = isbn;
+        }
+
+        public boolean isReaded() {
+            return readed;
+        }
+
+        public void setReaded(boolean readed) {
+            this.readed = readed;
+        }
+
+        public int getTimeReaded() {
+            return timeReaded;
+        }
+
+        public void setTimeReaded(int timeReaded) {
+            this.timeReaded = timeReaded;
+        }
+    }
+
+
